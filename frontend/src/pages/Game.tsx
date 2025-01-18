d3c0f32e75969cedc4c5c76eeb6833008d0b4d80
Soh Hong Yu <sohhongyu@gmail.com>
2025-01-18 13:57:25 +0800

    added base home page

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,3 +1,6 @@
+import { useParams } from "react-router-dom";
+
 export default function Game() {
+  const { game_id } = useParams();
   return <div>Game</div>;
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,75 @@
+import { useLocalStorageState } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import supabase from "@/lib/supabase";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const [userID, _] = useLocalStorageState<string>("user_id", uuidv4());
+  const [name, setName] = useLocalStorageState<string>("name", "");
+  const [gameID, setGameID] = useState<string>("");
+
+  useEffect(() => {
+    async function upsertUserData() {
+      const { data, error } = await supabase.from("art_users").upsert({
+        user_id: userID,
+        name: name,
+      });
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data);
+      return;
+    }
+
+    upsertUserData();
+  }, [userID, name]);
+
+  const handleEnterLobby = () => {
+    if (!name) {
+      return toast.error("Missing Name");
+    }
+
+    if (!gameID) {
+      return toast.error("Missing Game ID");
+    }
+    navigate(gameID);
+    return;
+  };
+
+  return (
+    <div>
+      <p>Home {userID}</p>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="name">Name</Label>
+        <Input
+          type="text"
+          id="name"
+          placeholder="Enter Name"
+          defaultValue={name}
+          onInput={(event) => {
+            setName((event.target as HTMLInputElement).value);
+          }}
+        />
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="roomID">Room ID</Label>
+        <Input
+          type="text"
+          id="roomID"
+          placeholder="Enter Room ID"
+          defaultValue={gameID}
+          onInput={(event) => {
+            setGameID((event.target as HTMLInputElement).value);
+          }}
+        />
+      </div>
+      <Button onClick={handleEnterLobby}>Enter</Button>
+    </div>
+  );
 }
